f94ba7d5f05eb738be41f9f7f57d81d9f8176dc0
Gerard Rovira <gerardrovira@meta.com>
2025-04-26 17:18:32 +0200

    center it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
     </>
   );
 }
